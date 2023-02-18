a7ce3ff02f173d9593e069cfc318d0cca5db4bf3
HuanLinMaster <linsmc@126.com>
2023-02-19 00:57:44 +0800

    build: fix: simplify code

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,15 +1,11 @@
 import { defineTheme } from '@koishijs/vitepress/client'
 import home from './layouts/home/index.vue'
-import market from './layouts/market/index.vue'
-import starter from './layouts/starter.vue'
 import Layout from './layout.vue'
 import './index.scss'
 
 export default defineTheme({
   layouts: {
     home,
-    market,
-    starter,
   },
   Layout,
 })
